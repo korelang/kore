8be5644bd7eb47aeac8c05c7cd3ffa28645cf93b
MisanthropicBit <albo.developer@gmail.com>
2021-04-28 17:52:42 +0200

    Add first test (with catch2)

---
+++ tests/scanner/test_literals.cpp
@@ -0,0 +1,49 @@
+#include "catch.hpp"
+
+#include "scanner.hpp"
+#include "location.hpp"
+
+void expect_token(
+    Token token,
+    TokenType type,
+    const std::string& value,
+    std::size_t lnum,
+    std::size_t start,
+    std::size_t end
+) {
+    REQUIRE(token.type() == type);
+    REQUIRE(token.value() == value);
+    REQUIRE(token.location().lnum() == lnum);
+    REQUIRE(token.location().start() == start);
+    REQUIRE(token.location().end() == end);
+}
+
+TEST_CASE("Can scan literals", "[literals]") {
+    Scanner scanner;
+    scanner.open_file("./tests/literals.kore");
+
+    expect_token(scanner.next_token(), TokenType::integer, "0xff", 1, 0, 3);
+    expect_token(scanner.next_token(), TokenType::integer, "0b1010011", 2, 0, 8);
+    expect_token(scanner.next_token(), TokenType::integer, "076314", 3, 0, 5);
+    expect_token(scanner.next_token(), TokenType::integer, "1", 4, 0, 0);
+    expect_token(scanner.next_token(), TokenType::integer, "10", 5, 0, 1);
+    expect_token(
+        scanner.next_token(),
+        TokenType::single_line_comment,
+        "# This is a floating-point number",
+        6,
+        0,
+        32
+    );
+    expect_token(scanner.next_token(), TokenType::floatp, "10.35", 7, 0, 4);
+    expect_token(scanner.next_token(), TokenType::character, "€", 8, 1, 1);
+    expect_token(scanner.next_token(), TokenType::string, "\"hello\"", 9, 0, 6);
+    expect_token(scanner.next_token(), TokenType::lbracket, "[", 10, 0, 0);
+    expect_token(scanner.next_token(), TokenType::integer, "1", 10, 2, 2);
+    expect_token(scanner.next_token(), TokenType::comma, ",", 10, 4, 4);
+    expect_token(scanner.next_token(), TokenType::integer, "2", 10, 6, 6);
+    expect_token(scanner.next_token(), TokenType::comma, ",", 10, 7, 7);
+    expect_token(scanner.next_token(), TokenType::integer, "3", 10, 8, 8);
+    expect_token(scanner.next_token(), TokenType::rbracket, "]", 10, 10, 10);
+    expect_token(scanner.next_token(), TokenType::eof, "", 10, 11, 11);
+}
