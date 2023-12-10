2728cebbe1cd4dec6bd4983ec82ecaa054fecb67
MisanthropicBit <albo.developer@gmail.com>
2023-12-10 16:47:50 +0100

    Special formatters for registers/constants

--- src/targets/bytecode/disassemble/instruction.cpp
+++ src/targets/bytecode/disassemble/instruction.cpp
@@ -1,12 +1,52 @@
 #include <sstream>
 #include "instruction.hpp"
+#include "targets/bytecode/register.hpp"
 
 namespace koredis {
     const std::string register_symbol = "@";
     const std::string constant_index_symbol = "#";
 
-    std::ostream& reg(std::ostream& os, kore::Reg reg) {
-        return os << register_symbol << reg;
+    struct reg {
+        kore::Reg _reg;
+
+        reg(kore::Reg reg) : _reg(reg) {}
+    };
+
+    struct regs {
+        const std::vector<kore::Reg>& _regs;
+
+        regs(const std::vector<kore::Reg>& regs) : _regs(regs) {}
+    };
+
+    struct constant {
+        int _index;
+
+        constant(int index) : _index(index) {}
+    };
+
+    template<typename CharT, typename Traits>
+    decltype(auto) operator<<(std::basic_ostream<CharT, Traits>& os, reg reg_formatter) {
+        return os << register_symbol << reg_formatter._reg;
+    }
+
+    template<typename CharT, typename Traits>
+    decltype(auto) operator<<(std::basic_ostream<CharT, Traits>& os, regs regs_formatter) {
+        auto& _regs = regs_formatter._regs;
+
+        for (std::size_t i = 0; i < _regs.size(); ++i) {
+            os << reg(_regs[i]);
+
+            if (i - 1 < _regs.size()) {
+                os << " ";
+            }
+        }
+
+        return os;
+    }
+
+    template<typename CharT, typename Traits>
+    decltype(auto) operator<<(std::basic_ostream<CharT, Traits>& os, constant constant_formatter) {
+        return os << constant_index_symbol << constant_formatter._index;
     }
 
     std::ostream& format_register_list(
@@ -27,18 +67,18 @@ namespace koredis {
         if (auto ins_type = std::get_if<kore::kir::OneRegister>(&instruction_type)) {
         } else if (auto ins_type = std::get_if<kore::kir::TwoRegisters>(&instruction_type)) {
             if (instruction.value.opcode == kore::Bytecode::Gstore) {
-                os << " " << constant_index_symbol << ins_type->reg1 << " " << register_symbol << ins_type->reg2;
+                os << " " << constant(ins_type->reg1) << " " << reg(ins_type->reg2);
             } else {
-                os << " " << register_symbol << ins_type->reg1 << " " << register_symbol << ins_type->reg2;
+                os << " " << reg(ins_type->reg1) << " " << reg(ins_type->reg2);
             }
         } else if (auto ins_type = std::get_if<kore::kir::ThreeRegisters>(&instruction_type)) {
-            os << " " << register_symbol << ins_type->reg1 << " "
-               << register_symbol << ins_type->reg2 << " "
-               << register_symbol << ins_type->reg3;
+            os << " " << reg(ins_type->reg1)
+               << " " << reg(ins_type->reg2)
+               << " " << reg(ins_type->reg3);
         } else if (auto ins_type = std::get_if<kore::kir::RegisterAndValue>(&instruction_type)) {
             auto opcode = instruction.value.opcode;
             auto value = ins_type->value;
-            os << " "; reg(os, ins_type->reg);
+            os << " " << reg(ins_type->reg);
 
             if (opcode == kore::Bytecode::JumpIf || opcode == kore::Bytecode::JumpIfNot) {
                 auto target_pos = instruction.byte_pos + value;
@@ -47,7 +87,7 @@ namespace koredis {
             } else if (opcode == kore::Bytecode::LoadBool) {
                 os << " " << (value == 1 ? "true" : "false");
             } else {
-                os << " " << constant_index_symbol << value;
+                os << " " << constant(value);
             }
         } else if (auto ins_type = std::get_if<kore::kir::Value>(&instruction_type)) {
             auto value = ins_type->value;
@@ -55,16 +95,15 @@ namespace koredis {
 
             os << " " << value << " [target: " << target_pos << "]";
         } else if (auto ins_type = std::get_if<kore::kir::CallV>(&instruction_type)) {
-            os << " " << register_symbol << ins_type->func_index;
-            format_register_list(os, ins_type->arg_registers);
-            format_register_list(os, ins_type->ret_registers);
-
-            os << " [args: "
+            os << " " << reg(ins_type->func_index)
+               << " " << regs(ins_type->arg_registers)
+               << " " << regs(ins_type->ret_registers)
+               << " [args: "
                << ins_type->arg_registers.size()
                << ", returns: "
                << ins_type->ret_registers.size() << "]";
         } else if (auto ins_type = std::get_if<kore::kir::ReturnV>(&instruction_type)) {
-            format_register_list(os, ins_type->registers);
+            os << regs(ins_type->registers);
         }
 
         return os;
